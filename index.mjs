fc6d4e91cca7f84ccffccee1481f747ba0ec7ed4
amirda <121286048+amirdaGankhuyag@users.noreply.github.com>
2023-12-20 11:30:12 +0800

    .

--- index.mjs
+++ index.mjs
@@ -33,11 +33,14 @@
 import data from "./anime-list.json" assert {type: "json"};
 import express from "express";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJsondoc from "swagger-jsdoc";
+
 const app = express();
 const port = 3000;
 
 const homePage = `<html><h1>Welcome home</h1><p>cmp</p></html>`;
-const aboutPage = `<html><h1>Welcome about</h1><p>ab</p></html>`;
+const aboutPage = `<html><h1 style="text-align: center">Welcome about</h1><p>ab</p></html>`;
 
 let newAnime = 0;
 
@@ -62,3 +65,30 @@ app.get("/animeList/:animeId", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// swagger documentation 3 deer hiihed l bolno gesen
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0", // present supported openapi version
+    info: {
+      title: "My API", // short title.
+      version: "1.0.0", // version number
+      description: "Animuk dev API for Web-app", // desc.
+      contact: {
+        name: "John doe", // your name
+        email: "john@web.com", // your email
+        url: "web.com", // your website
+      }
+    }
+  },
+  apis: ["./app.mjs"]
+};
+
+const specs = swaggerJsondoc(options); // Assuming you have imported swaggerJsondoc
+
+app.use("/docs", swaggerUi.serve);
+
+app.get("/docs", swaggerUi.setup(specs, {
+  explorer: true
+}));

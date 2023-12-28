87223137a9749d4fb5ee8fa808630fd05c304e7e
unubaatar <117814212+unubaatar@users.noreply.github.com>
2023-12-28 14:22:45 +0800

    asdf

--- app.mjs
+++ app.mjs
@@ -36,6 +36,44 @@ app.get("/", (req, res) => {
   res.sendFile("./front/login.html", options);
 });
 
+app.get("/signUp" , (req , res) => {
+  res.sendFile("./front/signUp.html" , options); 
+})
+
+app.post("/register" , async(req , res) => {
+  try {
+    const email = req.body.email;
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const phone = req.body.phone;
+    const password1 = req.body.password1;
+    const password2 = req.body.password2;
+    console.log(req.body);
+    const result1 = await client.query(`SELECT * FROM users WHERE email = $1`, [email]);
+    const result2 = await client.query(`SELECT * FROM users WHERE phone = $1`, [phone]);
+    if( result1.rowCount.length > 0 && result2.rowCount.length > 0) {
+      res.status(400).json({ error: 'Email or phone is already registered' });
+    } else if(password1 != password2) {
+      res.status(400).json({ error: 'Нууц үгээ зөв давтаж оруулна уу' });
+    } else {
+      const query = {
+        text: `INSERT INTO users (fname , lname , phone , email, password) VALUES($1, $2, $3, $4, $5)`,
+        values: [
+          fname,
+          lname,
+          phone,
+          email,
+          password1
+        ],
+      };
+      await client.query(query);
+      res.status(200).send("boloro hamayo");
+    }
+  } catch(err) {
+      console.error(err);
+  }
+})
+
 
 app.get("/test", async (req, res) => {
     try {
@@ -66,6 +104,7 @@ app.get("/productList" , async(req , res) => {
     res.status(500).json({ error: "Server error" });
   }
 })
+
 
 app.listen(port, () => {
   console.log("Server is listening on port: " + port);
